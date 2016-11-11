5ae1cb434c983111c9e970d2d45dabca728a917e
Sara OC <saramoo@hotmail.com>
2016-11-11 21:53:49 +0000

    fail visit /users/new test

---
+++ features/step_definitions/user_steps.rb
@@ -0,0 +1,15 @@
+Given(/^I am in the sign up section$/) do
+  visit '/users/new'
+end
+
+When(/^I fill in "([^"]*)" with "([^"]*)"$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I see "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
